8a9cc94ebcdf23b7314d4911038caa57df828766
Joseph Eaton <josepheaton04@outlook.com>
2024-11-13 12:18:41 +0000

    Add members to SafariEnv; add inheritance indicator to HuntingGround, Sky, Water.

--- Animals/Animals/SafariEnvironment/SafariEnvironment.cs
+++ Animals/Animals/SafariEnvironment/SafariEnvironment.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Animals.SafariEnvironment
 {
-    internal class SafariEnvironment
-    {
+    public abstract class SafariEnvironment<IEnvironment>
+        {
+            protected List<Animal> animalsInEnvironment = new List<Animal>();
+
+            public abstract void CheckAnimalsInEnvironment();
+
+            public void AddAnimalInEnvironment(Animal animal)
+            {
+                this.animalsInEnvironment.Add(animal);
+            }
+        }
     }
-}
+
