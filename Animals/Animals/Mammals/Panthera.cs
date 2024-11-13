67006c8002994494b93248b3357126bd0742a501
Joseph Eaton <josepheaton04@outlook.com>
2024-11-13 11:10:51 +0000

    Add Panthera superclass with MakeSound functionality.

---
+++ Animals/Animals/Mammals/Panthera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals.Mammals
+{
+    internal class Panthera
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine(this.GetType().Name + " roars!");
+        }
+    }
+}
--- Animals/Animals/obj/Debug/net8.0/Animals.AssemblyInfo.cs
+++ Animals/Animals/obj/Debug/net8.0/Animals.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Animals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+261e5c22348fac83732f54d89bc89585848cbf46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649217d2345c670841cbce11395ccc404dd38efb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Animals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Animals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
