f7c41b8a19a3f9360cbaac094451e083ee34c77c
bart <bielski.bart1012@gmail.com>
2024-11-13 10:47:02 +0000

    Create abstract Animal class

---
+++ Animals/Animals/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    abstract class Animal
+    {
+        public float Weight;
+
+        protected Animal(float weight)
+        {
+            Weight = weight;
+        }
+
+        public abstract void MakeSound();
+    }
+}
